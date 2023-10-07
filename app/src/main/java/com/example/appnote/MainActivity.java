a97030e7ab278ba4aaf943d24e2fe3ec1a0efbd6
IceMan0D <jangcom2562@gmail.com>
2023-10-07 17:25:01 +0700

    add code

--- app/src/main/java/com/example/appnote/MainActivity.java
+++ app/src/main/java/com/example/appnote/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("hello");
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
     }
 }
